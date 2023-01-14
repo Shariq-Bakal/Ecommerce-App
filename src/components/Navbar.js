f2f8bed19eb04d2a90508f3e8123654906fad19f
Shariq-Bakal <shariq.f.bakal@gmail.com>
2023-01-15 00:59:54 +0530

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar  = () => {
-  const {cart} = useContext(C)
+   
   return (
     <div> <div className='nav-bar'>
     
