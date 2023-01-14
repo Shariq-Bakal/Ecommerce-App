269b8f2c7c1ec1c5eea3f8e54a6f4808bf6d0613
Shariq-Bakal <shariq.f.bakal@gmail.com>
2023-01-14 07:54:01 +0530

    Refactor

--- src/pages/Wishlistpage.jsx
+++ src/pages/Wishlistpage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Wishlist } from '../contexts/Wishcontext'
 import { useContext } from 'react'
+import Navbar from '../components/Navbar'
 
 
 const Wishlistpage = () => {
@@ -17,12 +18,13 @@ function removewishlist(item){
  
   return (
     <div>
+        <Navbar/>
         <div className='wishlist-product-container'>
         <h2>My Wishlist:|{wishlist.length}|</h2>
         
         {
             wishlist.map(item=>{
-                return <div className="products"> <img height="70%" width="100%" src="https://m.media-amazon.com/images/I/61y7eNVNEeL._UY741_.jpg"/> <div className="productdetails"> price: ${item.price} <br/>Rating:{item.ratings } <br/> Description: {item.title} <br/>   </div><button onClick={()=>removewishlist(item)} 
+                return <div className="wishlist-products"> <img height="70%" width="100%" src="https://m.media-amazon.com/images/I/61y7eNVNEeL._UY741_.jpg"/> <div className="productdetails"> price: ${item.price} <br/>Rating:{item.ratings } <br/> Description: {item.title} <br/>   </div><button onClick={()=>removewishlist(item)} 
                 className="remove-wishlist">Remove from wishlist</button></div>
             })
         }
