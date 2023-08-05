1f41475ee13628d16131d267046ee063580ad6b0
Shariq Bakal <114148855+Shariq-Bakal@users.noreply.github.com>
2023-08-05 05:52:20 +0530

    style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ const Navbar  = () => {
       
     
       
-      <a className='nav-cart'><Link style={{textDecoration:"none",color:"black"}} to="/cartpage"><span class="material-symbols-outlined">shopping_bag</span> <span className='nav-text'>Bag</span></Link></a>
-      <a className='nav-wishlist'><Link style={{textDecoration:"none",color:"black"}} to="/wishlistpage"><span class="material-symbols-outlined">favorite</span><span className='nav-text'>Wishlist</span></Link></a>
+      <a className='nav-cart'><Link style={{textDecoration:"none",color:"white"}} to="/cartpage"><span class="material-symbols-outlined">shopping_bag</span> <span className='nav-text'>Bag</span></Link></a>
+      <a className='nav-wishlist'><Link style={{textDecoration:"none",color:"white"}} to="/wishlistpage"><span class="material-symbols-outlined">favorite</span><span className='nav-text'>Wishlist</span></Link></a>
       
     
     <a className='logo'><Link style={{textDecoration:"none",color:"black"}} to="/"><span className='logo-back'> StyleSphere</span></Link></a>
